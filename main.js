f22169d202443a383584c8b19a5c129c054ce35e
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2019-06-10 17:31:07 +0300

    toggle click event was added

--- main.js
+++ main.js
@@ -5,7 +5,17 @@ for (let i = 0; i < tabsTitle.length; i++) {
 }
 
 function tabClickHandler(e) {
-    this.parentElement.classList.toggle('js-active');    
+    this.parentElement.classList.toggle('js-active');
+
+    let tabs = document.querySelectorAll('.tab');
+    let tabsContent = document.querySelectorAll('.tab__content');
+
+    for (let i = 0; i < tabs.length; i++) {
+        if (tabs[i] !== this.parentElement) {
+            tabs[i].classList.remove('js-active');
+            tabsContent[i].style.maxHeight = null
+        }
+    }
 
     let currentTabContent = this.parentElement.querySelector('.tab__content');
 
